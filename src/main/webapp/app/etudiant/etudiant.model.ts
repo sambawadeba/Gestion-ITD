43036c39d13ae9a6dc97fa3885ad2cf73f58ae50
sambawadeba <sambawade.ba@itd-hub.com>
2024-09-02 22:21:50 +0000

    hello

---
+++ src/main/webapp/app/etudiant/etudiant.model.ts
@@ -0,0 +1,10 @@
+export class Etudiant {
+    constructor(
+      public id: number = 0,
+      public nom: string = '',
+      public prenom: string = '',
+      public adresse: string = '',
+      public email: string = '',
+      public telephone: string = ''
+){}
+  }
---
+++ src/main/webapp/app/etudiant/etudiant.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Etudiant } from './etudiant.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EtudiantService {
+  private baseUrl = 'http://localhost:8080/api/etudiants';
+
+  constructor(private http: HttpClient) { }
+
+  creerEtudiant(etudiant: Etudiant): Observable<Etudiant> {
+    return this.http.post<Etudiant>(`${this.baseUrl}`, etudiant);
+  }
+
+  mettreAJourEtudiant(id: number, infoEtudiant: Etudiant): Observable<Etudiant> {
+    return this.http.put<Etudiant>(`${this.baseUrl}/${id}`, infoEtudiant);
+  }
+
+  getEtudiant(id: number): Observable<Etudiant> {
+    return this.http.get<Etudiant>(`${this.baseUrl}/${id}`);
+  }
+  getEtudiants(): Observable<Etudiant[]> {
+    return this.http.get<Etudiant[]>(`${this.baseUrl}`);
+  }
+
+  supprimerEtudiant(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+}
+}
